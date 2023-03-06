39d1ad2a7627e23808f10d20d2f9186ef79e6b4c
cemtaskin <cemtaskin82@gmail.com>
2023-03-06 15:59:49 +0300

    state added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,36 @@
 import Card from "../UI/ Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import React,{useState} from 'react';
 
-const ExpenseItem = ({ title, amount, date })  => {
+const ExpenseItem = (props)  => {
+ 
+  const [title,setTitle] = useState(props.title);
+  const [counter,setCounter] = useState(0);
+
+
+  const clickHandler = ()=> {
+    console.log("Title changed:" + title);
+    setTitle("updated");
+    console.log("Title changed:" + title);
+  };
+
+  const clickCounterHandler = ()=> {
+   
+    setCounter(counter+1);
+   
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickCounterHandler}>{counter}</button>
+      
     </Card>
   );
 }
